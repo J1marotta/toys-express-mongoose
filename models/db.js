04543e31857252ca50d06f010bb64f261e2acaa2
Michael Bamford <michael@michaelbamford.com>
2017-11-23 21:10:13 +1100

    Capture and log mongoose connection error

--- models/db.js
+++ models/db.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/toys', { useMongoClient: true });
+
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/toys', { useMongoClient: true });
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 module.exports = mongoose;
