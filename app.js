3cb4dcf97f0a04388dfc30ac18a362396013023d
Michael Bamford <michael@michaelbamford.com>
2017-11-23 21:56:33 +1100

    Add morgan logger

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const routes = require('./routes');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.all('/', function (req, res, next) {
-  console.log('Accessing the secret section ...');
-  next(); // pass control to the next handler
-})
-
+app.use(morgan('combined'));
 app.use('/', routes);
 
 app.listen(3000, () =>
